15d1474fd15d1ff9a653779b67da962007d586e5
Scott Ertel <jsecpa@gmail.com>
2017-06-26 18:34:24 -0500

    add annotate gem

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,13 @@
 class Genre < ApplicationRecord
   has_many :artists
 end
+
+# == Schema Information
+#
+# Table name: genres
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
